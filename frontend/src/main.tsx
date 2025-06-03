a8187e2f1ed24a6d2522548b9d3c000ee17eee36
Tudor Carsin <110934835+blue892@users.noreply.github.com>
2025-06-03 06:51:12 -0400

    Install React Router, Tailwind, Axios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from "react-router";
+import { createRoot } from 'react-dom/client';
+import router from './routes.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+);
